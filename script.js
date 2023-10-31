e02765d1c8837c19080789178fdbe37e6d814366
hadexyz <142442544+hadexyz@users.noreply.github.com>
2023-10-30 23:41:32 -0300

    Add function getComputerChoice with a random number generator

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 console.log("script.js connected succesfully")
+
+
+function getComputerChoice() {
+    let RNG = Math.floor(Math.random() * 3 + 1);
+    console.log(`Random number generated:${RNG}`)
+}
 function playRound(playerSelection, computerSelection) {
     // your code here!
   }
