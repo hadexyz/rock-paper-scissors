9fbe329e55546ce0a5b0d95a1b858403f6ffbf79
hadexyz <142442544+hadexyz@users.noreply.github.com>
2023-10-30 23:36:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("script.js connected succesfully")
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
