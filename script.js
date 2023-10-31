780aa63e34fb3e261a48a152c8192996b6901b0f
hadexyz <142442544+hadexyz@users.noreply.github.com>
2023-10-30 23:48:59 -0300

    Add return string to each number generated in AI

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ console.log("script.js connected succesfully")
 function getComputerChoice() {
     let RNG = Math.floor(Math.random() * 3 + 1);
     console.log(`Random number generated:${RNG}`)
+    switch (RNG) {
+        case 1:
+            return("Rock");
+        case 2:
+            return("Paper");
+        case 3:
+            return("Scissor");
+    }
 }
 function playRound(playerSelection, computerSelection) {
     // your code here!
